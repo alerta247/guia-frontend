00a5ff0762151630931ed06e0bc3d5d409ffe928
jjanesmd-collab <jjanesmd@gmail.com>
2026-08-27 01:42:22 -0500

    fix: remove SANITY_API_READ_TOKEN build-time throw

--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -4,10 +4,7 @@
 import { defineLive } from "next-sanity";
 import { client } from './client'
 
-const token = process.env.SANITY_API_READ_TOKEN;
-if(!token) {
-  throw new Error("SANITY_API_READ_TOKEN is not set");
-}
+const token = process.env.SANITY_API_READ_TOKEN || '';
 
 export const { sanityFetch, SanityLive } = defineLive({
   client,
